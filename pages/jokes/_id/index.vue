c65faa20dc5ad591a8ca8e8602603611fa4ed5c7
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-04-10 16:33:24 +0100

    dad jokes

---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nuxt-link to="/jokes">Back To Jokes</nuxt-link>
+    <h2>
+      {{ joke }}
+    </h2>
+    <hr />
+    <small>Joke ID: {{$route.params.id}}</small>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      joke: {},
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+
+      this.joke = res.data.joke;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: this.$route.params.id
+    }
+  }
+};
+</script>
+
+<style></style>
